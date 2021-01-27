bd76434bbae38c13d5cff33ac345f8cd379217dc
Edward Scott <edward.scott@iotechsys.com>
2021-01-27 18:22:37 +0000

    Change command to use v1

--- AzureSphere/update.sh
+++ AzureSphere/update.sh
@@ -3,6 +3,6 @@
 HUBNAME=$1
 RESOURCE=$2
 VALUE=$3
-ID=`azsphere device list-attached | grep ID | cut -d' ' -f4 | tr '[A-Z]' '[a-z]'`
+ID=`azsphere_v1 device list-attached | grep ID | cut -d' ' -f4 | tr '[A-Z]' '[a-z]'`
 
 az iot hub invoke-device-method --hub-name ${HUBNAME} --device-id ${ID} --method-name publish  --method-payload "{\"device\":\"damocles-virt1\", \"values\": {\"${RESOURCE}\":${VALUE}}}"
