66a7ba52fe1c4f2cf92c3fc60bed7efaf233f5c4
Steve Osselton <steve@iotechsys.com>
2021-02-01 12:01:22 +0000

    Batch file fixes

--- AzureSphere/deployd.bat
+++ AzureSphere/deployd.bat
@@ -1,4 +1,4 @@
 call azsphere device sideload delete
 call azsphere device sideload deploy --image-package build/xrt-app.imagepackage
-call azsphere device app stop --componentid 4a1f4b12-bf60-4dc3-8ce9-bc585c009c5f
-call azsphere device app start --debug --componentid 4a1f4b12-bf60-4dc3-8ce9-bc585c009c5f
+call azsphere device app stop --component-id 4a1f4b12-bf60-4dc3-8ce9-bc585c009c5f
+call azsphere device app start --debug --component-id 4a1f4b12-bf60-4dc3-8ce9-bc585c009c5f
