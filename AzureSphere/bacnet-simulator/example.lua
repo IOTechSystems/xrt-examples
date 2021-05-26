75e68631d666ddc23fafe5c3cc27d95a60aac468
Luke <luke@iotechsys.com>
2021-05-26 17:08:02 +0100

    XRT-586 Remove sleep from bacnet lua script

--- AzureSphere/bacnet-simulator/example.lua
+++ AzureSphere/bacnet-simulator/example.lua
@@ -10,14 +10,9 @@ instances["BinaryValue"] = {total = 1, readOnly = false}
 instances["PositiveIntegerValue"] = {total = 1, readOnly = false}
 instances["Accumulator"] = {total = 1, readOnly = false}
 
-binaryOptions = {0, 1, 255}
+local binaryOptions = {0, 1, 255}
 
-function sleep(time)
-  local duration = os.time() + time
-  while os.time() < duration do end
-end
-
-function Run() --run once
+function Run()
   for instanceType, instance in pairs(instances) do
     if instance.total > 0 then
       print("Create " .. instance.total .. " " .. instanceType .. " instances")
@@ -25,11 +20,16 @@ function Run() --run once
       bacnet["create" .. instanceType .. "s"](instance.total)
     end
   end
+end
 
-  local increment = 0
-  local binaryIncrement = 1
+local increment = 0
+local binaryIncrement = 1
 
-  while isBacnetRunning() do
+local waitDuration = 5 -- Update every 5 secs
+local lastUpdate = os.time()
+
+function Update()
+  if os.time() - lastUpdate > waitDuration then
     for instanceType, instance in pairs(instances) do
       if instance.total > 0 and instance.readOnly == true then
 	for instanceIterator = 0, instance.total - 1 do
@@ -52,10 +52,7 @@ function Run() --run once
     if binaryIncrement == 4 then
       binaryIncrement = 1
     end
-
-    sleep(1)
+    lastUpdate = os.time()
   end
 end
 
-function Update()
-end
