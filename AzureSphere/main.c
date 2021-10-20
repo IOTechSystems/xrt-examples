a7b10ccb145263faa9954b1b96b2cdb0c9b7a3cd
Hans Chen <hans@iotechsys.com>
2021-10-20 14:43:55 +0800

    add Influxdb example.

--- AzureSphere/main.c
+++ AzureSphere/main.c
@@ -47,6 +47,7 @@ int main (void)
   iot_component_factory_add (xrt_log_exporter_factory ());
   iot_component_factory_add (xrt_mt3620_device_factory ());
   iot_component_factory_add (xrt_azuresphere_exporter_factory ());
+  iot_component_factory_add (xrt_influxdb_exporter_factory ());
 #ifdef DEVICE_MODBUS
   iot_component_factory_add (xrt_modbus_device_service_factory ());
 #endif
