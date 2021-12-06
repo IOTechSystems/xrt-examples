4bb9a2a636cf297880031dad76f396cf17df0a79
Joe Riemersma <joepaz200@gmail.com>
2021-12-06 09:57:31 +0000

    XRT-1056: Removed readlink/dirname command from set_env_vars script

--- DeviceServices/opc-ua/commands/set_env_vars.sh
+++ DeviceServices/opc-ua/commands/set_env_vars.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 
-SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
-
 export OPCUA_SIM_ADDRESS=localhost:49947
 export OPCUA_LDS_ADDRESS=localhost:4840
 
-export XRT_PROFILE_DIR=$SCRIPT_DIR/../profiles/
-export XRT_STATE_DIR=$SCRIPT_DIR/../state/
+export XRT_PROFILE_DIR=$PWD/profiles/
+export XRT_STATE_DIR=$PWD/state/
 
 export XRT_MQTT_BROKER=tcp://localhost:1883
 export XRT_MQTT_USERNAME=""
