55a3cc27267694fee8aa755b1076b2ccd4c579ad
Joe Riemersma <joepaz200@gmail.com>
2022-01-19 15:15:43 +0000

    XRT-1193: added initial ble examples

---
+++ DeviceServices/ble/commands/start_device_sim.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+docker run --rm -d --name=ble-sim \
+      -e RUN_BLUEZ=true \
+      -e DEVICE_COUNT=1 \
+      --privileged \
+      -v /var/run/dbus/system_bus_socket/:/var/run/dbus/system_bus_socket/ \
+      -v /etc/dbus-1/system.d/:/etc/dbus-1/system.d \
+      --mount type=bind,source=/proc/1/ns/,target=/rootns \
+      iotechsys/ble-sim:1.0.1.dev \
+      --script /example-scripts/test-device.lua 
+
