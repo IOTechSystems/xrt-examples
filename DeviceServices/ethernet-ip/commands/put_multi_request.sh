fca38e38346f75d879678e4d9f42d8f2c168a98c
amaan-akram <amaan7akram@gmail.com>
2022-01-21 15:59:16 +0000

    XRT-1158: updated examples for v2 sdk

--- DeviceServices/ethernet-ip/commands/put_multi_request.sh
+++ DeviceServices/ethernet-ip/commands/put_multi_request.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
-mosquitto_pub -t xrt/device/ethernet_ip_device_service/request -m \
+mosquitto_pub -t xrt/device/ethernet_ip/request -m \
 '{
   "client": "example",
   "request_id": "1030",
   "op": "device:put",
   "device": "ethernetip-sim",
   "values": {
-    "DI1": false,
-    "DI2": true,
-    "DI3": true,
-    "DI4": true
+    "DO1": false,
+    "DO2": true,
+    "DO3": true,
+    "DO4": true
   }
 }'
