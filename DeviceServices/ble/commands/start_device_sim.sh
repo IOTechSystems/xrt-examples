90a7e1ce17cb57b8b0037f029cd0eb3f45ba548a
Joe Riemersma <joepaz200@gmail.com>
2022-01-21 12:25:05 +0000

    XRT-1193: Updated ble start_sim command to use new lua-script

--- DeviceServices/ble/commands/start_device_sim.sh
+++ DeviceServices/ble/commands/start_device_sim.sh
@@ -8,5 +8,5 @@ docker run --rm -d --name=ble-sim \
       -v /etc/dbus-1/system.d/:/etc/dbus-1/system.d \
       --mount type=bind,source=/proc/1/ns/,target=/rootns \
       iotechsys/ble-sim:1.0.1.dev \
-      --script /example-scripts/test-device.lua 
-
+      --script /example-scripts/device-service-examples.lua 
+      
