203a0a9b939eb57701ae704a8b6bc2823400216d
Amaan <43187649+amaan-akram@users.noreply.github.com>
2022-01-24 11:25:42 +0000

    XRT-1202: updated API & SDK version numbers - windows

--- AzureSphere/build.bat
+++ AzureSphere/build.bat
@@ -10,8 +10,8 @@ mkdir build
 cd build
 cmake -G "Ninja" ^
   -DCMAKE_TOOLCHAIN_FILE="C:/Program Files (x86)/Microsoft Azure Sphere SDK/CMakeFiles/AzureSphereToolchain.cmake" ^
-  -DAZURE_SPHERE_TARGET_TOOLS_REVISION="21.02" ^
-  -DAZURE_SPHERE_TARGET_API_SET="8" ^
+  -DAZURE_SPHERE_TARGET_TOOLS_REVISION="21.10" ^
+  -DAZURE_SPHERE_TARGET_API_SET="10" ^
   -DDEVICE="%device%" ^
   -DBOARD="%board%" ^
   -DCMAKE_BUILD_TYPE="Debug" ..
