5a3b628150c620f9f33942c7a3c5274217198aa5
amaan-akram <amaan7akram@gmail.com>
2022-01-24 12:14:16 +0000

    XRT-1202: typo changed 10 to 11

--- AzureSphere/build.bat
+++ AzureSphere/build.bat
@@ -11,7 +11,7 @@ cd build
 cmake -G "Ninja" ^
   -DCMAKE_TOOLCHAIN_FILE="C:/Program Files (x86)/Microsoft Azure Sphere SDK/CMakeFiles/AzureSphereToolchain.cmake" ^
   -DAZURE_SPHERE_TARGET_TOOLS_REVISION="21.10" ^
-  -DAZURE_SPHERE_TARGET_API_SET="10" ^
+  -DAZURE_SPHERE_TARGET_API_SET="11" ^
   -DDEVICE="%device%" ^
   -DBOARD="%board%" ^
   -DCMAKE_BUILD_TYPE="Debug" ..
