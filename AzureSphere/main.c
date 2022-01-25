ca96fbba66df70de86e1921b50259dd8a9cb9297
Edward Scott <edward.scott@iotechsys.com>
2022-01-25 09:24:38 +0000

    XRT-910: Updates for Azure Sphere

--- AzureSphere/main.c
+++ AzureSphere/main.c
@@ -27,95 +27,10 @@ extern void bip_debug_enable (void);
 /* Shutdown TERM signal handling */
 
 #define MALLOC_BLOCK_SIZE 512
-#define IOT_LOG_LEVELS 6
 
 static pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 static char * config = NULL;
 static iot_data_t * map = NULL;
-static const char * iot_log_levels[IOT_LOG_LEVELS] = {"", "ERROR", "WARN", "Info", "Debug", "Trace"};
-
-static void log_to_udp_broadcast (iot_logger_t * logger, iot_loglevel_t level, uint64_t timestamp, const char * message)
-{
-  static char str[1400];
-  static const char * iot_log_levels[IOT_LOG_LEVELS] = {"", "ERROR", "WARN", "Info", "Debug", "Trace"};
-  struct sockaddr_in broadcast_addr;
-  int sock = socket(AF_INET, SOCK_DGRAM, 0);
-  int yes = 1;
-  const char * sep = strchr (logger->to, ':');
-  memset((void*) &broadcast_addr, 0, sizeof(struct sockaddr_in));
-  broadcast_addr.sin_family = AF_INET;
-  if (sep)
-  {
-    char target[17];
-    strncpy (target, logger->to, (size_t) (sep - logger->to));
-    inet_aton (target, &broadcast_addr.sin_addr);
-    broadcast_addr.sin_port = htons((uint16_t) atoi (sep + 1));
-  }
-  else
-  {
-    broadcast_addr.sin_addr.s_addr = htonl(INADDR_BROADCAST);
-    broadcast_addr.sin_port = htons((uint16_t) atoi (logger->to));
-  }
-  iot_component_lock (&logger->component);
-  int len = snprintf (str, sizeof (str), "[%" PRIu64 ":%s:%s] %s\n", timestamp, logger->name, iot_log_levels[level], message);
-  if (len < 0)
-  {
-    perror ("snprintf error");
-    goto DONE;
-  }
-  int ret = setsockopt(sock, SOL_SOCKET, SO_BROADCAST, (char*)&yes, sizeof(yes));
-  if (ret == -1)
-  {
-    perror ("setsockopt error");
-    goto DONE;
-  }
-  ret = sendto (sock, str, (size_t) len, 0, (struct sockaddr*) &broadcast_addr, sizeof (struct sockaddr_in) );
-  if (ret < 0)
-  {
-    perror ("sendto error");
-  }
-  DONE:
-  iot_component_unlock (&logger->component);
-  close (sock);
-}
-
-static iot_loglevel_t iot_logger_config_level (const iot_data_t * map)
-{
-  iot_loglevel_t level = IOT_LOGLEVEL_DEFAULT;
-  const char * name = iot_data_string_map_get_string (map, "Level");
-  if (name)
-  {
-    int c = toupper (name[0]);
-    for (int lvl = 0; lvl < IOT_LOG_LEVELS; lvl++)
-    {
-      if (iot_log_levels[lvl][0] == c)
-      {
-        level = lvl;
-        break;
-      }
-    }
-  }
-  return level;
-}
-
-static iot_component_t * udp_logger_config (iot_container_t * cont, const iot_data_t * map)
-{
-  iot_logger_t * next;
-  iot_logger_t * logger;
-  iot_loglevel_t level = iot_logger_config_level (map);
-  const char * to = iot_data_string_map_get_string (map, "To");
-  next = (iot_logger_t*) iot_container_find_component (cont, iot_data_string_map_get_string (map, "Next"));
-  bool self_start = iot_data_string_map_get_bool (map, "Start", true);
-  printf("udp_logger_config: To = %s\n", to);
-  logger = iot_logger_alloc_custom (iot_data_string_map_get_string (map, "Name"), level, to, log_to_udp_broadcast, next, self_start);
-  return &logger->component;
-}
-
-static const iot_component_factory_t * udp_logger_factory (void)
-{
-  static iot_component_factory_t factory = { "XRT::UDPLogger", udp_logger_config, (iot_component_free_fn_t) iot_logger_free,  NULL, NULL };
-  return &factory;
-}
 
 static void termination_handler (int signal)
 {
@@ -210,7 +125,6 @@ int main (void)
   iot_component_factory_add (xrt_virtual_device_service_factory ());
 #endif
 
-  iot_component_factory_add (udp_logger_factory ());
   iot_container_init (container);
   free_load_config ();
 
