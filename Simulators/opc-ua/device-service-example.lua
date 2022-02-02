264d11348c4fb0b1afff64ca31d04c5b47eaabb3
Joe Riemersma <joe@iotechsys.com>
2022-02-02 14:01:21 +0000

    XRT-1222: Updated opc-ua device-service-example (#85)
    * XRT-1222: Updated opc-ua device-service-example
    
    * XRT-1222: Fixed issue where opc-ua sim script trigger event would not be called

--- Simulators/opc-ua/device-service-example.lua
+++ Simulators/opc-ua/device-service-example.lua
@@ -23,18 +23,18 @@ function sign(x)
   return x>0 and 1 or x<0 and -1 or 0
 end
 
---rounds to n decimal places
-function round(x, n)
- return tonumber(string.format("%." .. (n or 0) .. "f", x))
-end
-
 --create the namespaces
 ns = Server.addNamespace("SimulationServer") --creates namespace 2
 ns3 = Server.addNamespace("SimulationNodes") --creates namespace 3
 
 --create the folder to store the nodes under
 simulation_folder = ObjectNode.newRootFolder("Simulation", ns3) 
+datatypes_folder = ObjectNode.newRootFolder("DataTypes", ns3) 
+events_folder = ObjectNode.newRootFolder("Events", ns3)
+
 Server.addObjectNode (simulation_folder)
+Server.addObjectNode (datatypes_folder)
+Server.addObjectNode (events_folder)
 
 --initialise each node
 counter = VariableNode.newString("Counter", ns3, simulation_folder:getNodeId(), 0, DataType.DOUBLE, AccessLevel.READ)
@@ -44,20 +44,56 @@ sinusoid = VariableNode.newString("Sinusoid", ns3, simulation_folder:getNodeId()
 square = VariableNode.newString("Square", ns3, simulation_folder:getNodeId(), 0, DataType.DOUBLE, AccessLevel.READ)
 triangle = VariableNode.newString("Triangle", ns3, simulation_folder:getNodeId(), 0, DataType.DOUBLE, AccessLevel.READ)
 
-writeable_1 = VariableNode.newString("WritableInt64", ns, simulation_folder:getNodeId(), 0, DataType.INT64, AccessLevel.READ | AccessLevel.WRITE)
-writeable_2 = VariableNode.newString("WritableFloat", ns, simulation_folder:getNodeId(), 0.0, DataType.FLOAT, AccessLevel.READ | AccessLevel.WRITE)
-writeable_3 = VariableNode.newString("WritableString", ns, simulation_folder:getNodeId(), "", DataType.STRING, AccessLevel.READ | AccessLevel.WRITE)
+--Create a variable node of each supported type
+bool = VariableNode.newString("Bool", ns3, datatypes_folder:getNodeId(), true, DataType.BOOL, AccessLevel.READ | AccessLevel.WRITE)
+sbyte = VariableNode.newString("SByte", ns3, datatypes_folder:getNodeId(), -128, DataType.SBYTE, AccessLevel.READ | AccessLevel.WRITE)
+byte = VariableNode.newString("Byte", ns3, datatypes_folder:getNodeId(), 255, DataType.BYTE, AccessLevel.READ | AccessLevel.WRITE)
+int16 = VariableNode.newString("Int16", ns3, datatypes_folder:getNodeId(), -32768, DataType.INT16, AccessLevel.READ | AccessLevel.WRITE)
+uint16 = VariableNode.newString("UInt16", ns3, datatypes_folder:getNodeId(), 65535, DataType.UINT16, AccessLevel.READ | AccessLevel.WRITE)
+int32 = VariableNode.newString("Int32", ns3, datatypes_folder:getNodeId(), -2147483648, DataType.INT32, AccessLevel.READ | AccessLevel.WRITE)
+uint32 = VariableNode.newString("UInt32", ns3, datatypes_folder:getNodeId(), 4294967295, DataType.UINT32, AccessLevel.READ | AccessLevel.WRITE)
+int64 = VariableNode.newString("Int64", ns3, datatypes_folder:getNodeId(), -9223372036854775808, DataType.INT64, AccessLevel.READ | AccessLevel.WRITE)
+uint64 = VariableNode.newString("UInt64", ns3, datatypes_folder:getNodeId(), 9223372036854775807, DataType.UINT64, AccessLevel.READ | AccessLevel.WRITE) --lua does not support uint64
+float = VariableNode.newString("Float", ns3, datatypes_folder:getNodeId(), 123456.78, DataType.FLOAT, AccessLevel.READ | AccessLevel.WRITE)
+double = VariableNode.newString("Double", ns3, datatypes_folder:getNodeId(), 109876543.21, DataType.DOUBLE, AccessLevel.READ | AccessLevel.WRITE)
+string = VariableNode.newString("String", ns3, datatypes_folder:getNodeId(), "Example string data type", DataType.STRING, AccessLevel.READ | AccessLevel.WRITE)
+datetime = VariableNode.newString("Datetime", ns3, datatypes_folder:getNodeId(), os.time(os.date("!*t")), DataType.DATETIME, AccessLevel.READ | AccessLevel.WRITE)
 
---add the nodes to the server
+--add the datatype nodes with the server
+Server.addVariableNode (bool)
+Server.addVariableNode (sbyte)
+Server.addVariableNode (byte)
+Server.addVariableNode (int16)
+Server.addVariableNode (uint16)
+Server.addVariableNode (int32)
+Server.addVariableNode (uint32)
+Server.addVariableNode (int64)
+Server.addVariableNode (uint64)
+Server.addVariableNode (float)
+Server.addVariableNode (double)
+Server.addVariableNode (string)
+Server.addVariableNode (datetime)
+
+--add the simulation nodes to the server
 Server.addVariableNode (counter)
 Server.addVariableNode (random)
 Server.addVariableNode (sawtooth)
 Server.addVariableNode (sinusoid)
 Server.addVariableNode (square)
 Server.addVariableNode (triangle)
-Server.addVariableNode (writeable_1)
-Server.addVariableNode (writeable_2)
-Server.addVariableNode (writeable_3)
+
+--Create event type nodes
+event_type_node_id = NodeId.newString ("SimpleEventType", 0) --We need to create the Node ID of our new type
+base_event_type_node_id = NodeId.newNumeric(ObjectType.BASEEVENTTYPE, 0) --We need to get the Node ID of the parent node of our new type - In this case this is the 'BASEEVENTTYPE'
+event_type_node = ObjectTypeNode.new(event_type_node_id, "SimpleEventType", base_event_type_node_id) --Actually create our Object Type node
+
+--Event origin node, this is the node that will produce the notification
+event_origin_node = ObjectNode.newFolder ("EventOriginNode", ns3, events_folder:getNodeId())
+event_origin_node:setEventNotifier(true) --For an Object node to be able to produce notifications we need to set this to true
+
+--Add our nodes to the server
+Server.addObjectTypeNode(event_type_node)
+Server.addObjectNode(event_origin_node)
 
 --update the nodes values every 1 second
 function Update()
@@ -66,24 +102,26 @@ function Update()
     return
   end
   t = t + 5
-
   --counter
   local counter_val = counter:getValue()
   counter_val = counter_val + 1
   counter:updateValue(counter_val)
   --random
-  local random_val = round(math.random(-20000,20000) / 10000, 8)
+  local random_val = math.random(-20000,20000) / 10000
   random:updateValue(random_val)
   --sawtooth
-  local sawtooth_val = round(2 * (t % (2*math.pi) * 1/math.pi - 1), 8)
+  local sawtooth_val = 2 * (t % (2*math.pi) * 1/math.pi - 1)
   sawtooth:updateValue(sawtooth_val)
   --sinusoid
-  local sinusoid_val = round(2 * math.sin(t), 8)
+  local sinusoid_val = 2 * math.sin(t)
   sinusoid:updateValue(sinusoid_val)
   --square
   local square_val = sign(math.sin(t)) * 2
   square:updateValue(square_val)
   --triangle
-  local triangle_val = round(2 * math.abs((t-math.pi/2) % (2*math.pi) * 1/math.pi - 1) - 1, 8)
+  local triangle_val = 2 * math.abs((t-math.pi/2) % (2*math.pi) * 1/math.pi - 1) - 1
   triangle:updateValue(triangle_val)
+
+  Server.triggerEvent(event_type_node, event_origin_node, 100, "AN EVENT MESSAGE", "Lua script")
+
 end
