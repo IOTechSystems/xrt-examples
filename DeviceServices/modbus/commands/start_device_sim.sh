9ab6b5e99bff224c79750526b08df3e06266dcdf
PMacald <peter.i.macaldowie@gmail.com>
2022-02-04 13:53:08 +0000

    XRT-1216: Moved RTU example and updated profile

--- DeviceServices/modbus/commands/start_device_sim.sh
+++ DeviceServices/modbus/commands/start_device_sim.sh
@@ -4,8 +4,7 @@ if [ "$RTU_MODE" = "true" ]
 then
     export MODBUS_SIM_ADDRESS="/dev/ttyUSB0"
     docker run --rm -d --name modbus-sim --network=host iotechsys/dev-testing-edgex-modbus-simulator:2.0.0 -protocol=RTU
-#<!-- todo: set Modbus Device address here and use later -->
-    #<-- todo: verify that this approach works or whether we need socat commands -->
+    # todo: get this running and test with new code
 else
     export MODBUS_SIM_PORT=1502
     docker run --rm -d --name modbus-sim -p $MODBUS_SIM_PORT iotechsys/dev-testing-edgex-modbus-simulator:2.0.0
