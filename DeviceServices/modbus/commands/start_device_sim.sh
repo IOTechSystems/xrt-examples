811f61eb42d3f0428d1b32445503bb2800567527
PMacald <peter.i.macaldowie@gmail.com>
2022-02-07 12:24:24 +0000

    XRT-1216: Started trying to get RTU sim working

--- DeviceServices/modbus/commands/start_device_sim.sh
+++ DeviceServices/modbus/commands/start_device_sim.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 
-if [ "$RTU_MODE" = "true" ]
-then
-    export MODBUS_SIM_ADDRESS="/dev/ttyUSB0"
-    docker run --rm -d --name modbus-sim --network=host iotechsys/dev-testing-edgex-modbus-simulator:2.0.0 -protocol=RTU
-    # todo: get this running and test with new code
+if [ "$RTU_MODE" = "true" ]; then
+  docker run --rm -d -v $PWD/commands/rtu-sim.sh:/rtu-sim.sh --entrypoint /rtu-sim.sh --name modbus-sim --network=host iotechsys/dev-testing-edgex-modbus-simulator:2.0.0
+  export MODBUS_SIM_ADDRESS=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' modbus-sim)
 else
-    export MODBUS_SIM_PORT=1502
-    docker run --rm -d --name modbus-sim -p $MODBUS_SIM_PORT iotechsys/dev-testing-edgex-modbus-simulator:2.0.0
-    export MODBUS_SIM_ADDRESS=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' modbus-sim)
+  export MODBUS_SIM_PORT=1502
+  docker run --rm -d --name modbus-sim -p $MODBUS_SIM_PORT iotechsys/dev-testing-edgex-modbus-simulator:2.0.0
+  export MODBUS_SIM_ADDRESS=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' modbus-sim)
 fi
