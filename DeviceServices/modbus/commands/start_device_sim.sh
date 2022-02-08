e6107787fa3931ebf3ce4a757d817064e9da2435
PMacald <peter.i.macaldowie@gmail.com>
2022-02-08 13:05:24 +0000

    XRT-1216: Further work on RTU sim, still not totally functional

--- DeviceServices/modbus/commands/start_device_sim.sh
+++ DeviceServices/modbus/commands/start_device_sim.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 if [ "$RTU_MODE" = "true" ]; then
-  docker run --rm -d -v $PWD/commands/rtu-sim.sh:/rtu-sim.sh --entrypoint /rtu-sim.sh --name modbus-sim --network=host iotechsys/dev-testing-edgex-modbus-simulator:2.0.0
+  docker run --rm -d -v $PWD/commands/rtu-sim.sh:/rtu-sim.sh --entrypoint /rtu-sim.sh --name modbus-sim iotechsys/dev-testing-edgex-modbus-simulator:2.0.0
   export MODBUS_SIM_ADDRESS=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' modbus-sim)
+  socat pty,link=/tmp/virtualport,raw,echo=0 tcp:$MODBUS_SIM_ADDRESS:55000
 else
   export MODBUS_SIM_PORT=1502
   docker run --rm -d --name modbus-sim -p $MODBUS_SIM_PORT iotechsys/dev-testing-edgex-modbus-simulator:2.0.0
