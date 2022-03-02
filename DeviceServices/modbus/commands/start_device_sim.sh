2d8a76ffe3bb16014f7a8a71660c68b038fa424d
Peter Macaldowie <peter.i.macaldowie@gmail.com>
2022-03-02 10:11:57 +0000

    XRT-1216: Updated if/else statements to use proper formatting

--- DeviceServices/modbus/commands/start_device_sim.sh
+++ DeviceServices/modbus/commands/start_device_sim.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-if [ "$1" = "rtu" ] then
+if [ "$1" = "rtu" ] 
+then
   docker run --rm -d -v $PWD/commands/rtu-sim.sh:/rtu-sim.sh --entrypoint /rtu-sim.sh --name modbus-sim iotechsys/dev-testing-edgex-modbus-simulator:2.0.0
   sleep 6
   MODBUS_SIM_ADDRESS=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' modbus-sim)
