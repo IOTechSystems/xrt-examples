da8c9cae545acc14cecfcfda2fdc4e92fad7423c
tylerg2471 <tylerg2471@gmail.com>
2022-03-16 14:19:33 +0000

    XRT-1331: Example updates for using device protocols

--- DeviceServices/gps/commands/add_device.sh
+++ DeviceServices/gps/commands/add_device.sh
@@ -8,6 +8,17 @@ mosquitto_pub -t xrt/devices/gps/request -m \
   "device": "gps-sim",
   "device_info": 
   {
-    "profileName": "GPS-Device"
+    "profileName": "GPS-Device",
+      "protocols": {
+        "GPS": {
+          "GpsdConnTimeoutMS": 2000,
+          "GpsdConnTimeoutS": 3,
+          "GpsdHostname": "0.0.0.0",
+          "GpsdMode": "poll",
+          "GpsdPort": 2947,
+          "GpsdRequestTimeout": 5000,
+          "GpsdRetries": 5
+        }
+      }
   }  
 }'
