f6f4ebf08316cc3ff01146bdded4b6140ea987ce
tylerg2471 <tylerg2471@gmail.com>
2022-03-24 15:12:30 +0000

    XRT-1179: Updated device sim start script and updated schedules to have more resources

--- DeviceServices/gps/commands/start_device_sim.sh
+++ DeviceServices/gps/commands/start_device_sim.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-GPS_SIM=iotechsys/gps-sim:1.0.0.dev
-docker pull $GPS_SIM
-docker run --rm -d --name gps-sim -p 2947:2947 --network=host $GPS_SIM
+GPS_SIM=knowhowlab/gpsd-nmea-simulator
+
+docker run --rm -d --name gps-sim -p 2947:2947 $GPS_SIM
