04f80558631a8fd11def0054350f94be97afc5a1
FraserG01 <fraser@iotechsys.com>
2022-05-03 10:58:51 +0100

    XRT-1446: updated opcua sim version

--- DeviceServices/opc-ua/commands/start_device_sim.sh
+++ DeviceServices/opc-ua/commands/start_device_sim.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-OPC_UA_SIM=iotechsys/opc-ua-sim:1.0.dev
+OPC_UA_SIM=iotechsys/opc-ua-sim:1.1
 docker pull $OPC_UA_SIM
 docker run --rm -d --name opc-ua-sim -e RUN_LDS=true --network=host -p 49947 -p 4840 $OPC_UA_SIM -l /example-scripts/device-service-example.lua
