597198840f06cacdee4ccf7d67feab72a344d923
Tyler Gibney <tylerg2471@gmail.com>
2022-11-16 14:48:16 +0000

    XRT-1943: Profile update for s7, added multi get/put scripts

---
+++ DeviceServices/s7/commands/multi_get_request.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+mosquitto_pub -t xrt/devices/s7/request -m \
+'{
+  "client": "example",
+  "request_id": "1050",
+  "op": "device:get",
+  "type": "xrt.request:1.0",
+  "device": "s7-sim",
+  "resource": [
+    "DB_1_UI8",
+    "DB_1_UI16",
+    "DB_1_UI32",
+    "DB_1_UI64",
+    "DB_1_I8",
+    "DB_1_I16",
+    "DB_1_I32",
+    "DB_1_I64",
+    "DB_1_F32",
+    "DB_1_F64",
+    "DB_1_String_Test"
+  ] 
+}'
