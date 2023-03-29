5c0b435837d26b564b344e8dd5ffebe7fb4aecb8
tylerg2471 <tylerg2471@gmail.com>
2023-03-29 13:50:57 +0100

    XRT2209: Added bacnet IP, modbus TCP and virtual services to opcua server basic examples

--- set_env_vars.sh
+++ set_env_vars.sh
@@ -8,8 +8,18 @@ export XRT_MQTT_USERNAME=""
 export XRT_MQTT_PASSWORD=""
 
 # Just used for ApplicationComponent Example
-export IOT_DIR=/opt/iotech/iot/1.4
+export IOT_DIR=/opt/iotech/iot/1.5
 export XRT_DIR=/opt/iotech/xrt/2.1
+
+# OPC-UA Server Basic Profile and State directories
+export XRT_VIRTUAL_PROFILE_DIR=../../../DeviceServices/virtual/deployment/profiles
+export XRT_VIRTUAL_STATE_DIR=../../../DeviceServices/virtual/deployment/state
+
+export XRT_BACNET_IP_PROFILE_DIR=../../../DeviceServices/bacnet-ip/deployment/profiles
+export XRT_BACNET_IP_STATE_DIR=../../../DeviceServices/bacnet-ip/deployment/state
+
+export XRT_MODBUS_TCP_PROFILE_DIR=../../../DeviceServices/modbus-tcp/deployment/profiles
+export XRT_MODBUS_TCP_STATE_DIR=../../../DeviceServices/modbus-tcp/deployment/state
 
 # OPC-UA server node modelling example 
 export XRT_MODELS_DIR=$PWD/deployment/models
