43fa065b6a6dd36d497186f0a3ab8b237fecf59f
Tyler Gibney <tylerg2471@gmail.com>
2023-05-10 18:04:52 +0100

    XRT-2268: PRofile, device script and flow updates

--- Tutorials/ChemicalTank/scripts/mqtt.lua
+++ Tutorials/ChemicalTank/scripts/mqtt.lua
@@ -1,7 +1,7 @@
 function modbus_sub_callback (data)
   newData = {}
   newData.device = {}
-  newData.device = "modbus-sim"
+  newData.device = "ChemicalTank"
   newData.readings = data.result.readings
   newData.request_id = data.request_id
 
@@ -12,7 +12,7 @@ function s7_sub_callback (data)
   if data.result.readings then
     newData = {}
     newData.device = {}
-    newData.device = "S7-Server"
+    newData.device = "OutletValve"
     newData.readings = data.result.readings
     newData.request_id = data.request_id
 
@@ -20,7 +20,7 @@ function s7_sub_callback (data)
   end
 end
 
-sub = sub or sub_alloc (iot_bus, modbus_sub_callback, "xrt/devices/modbus/reply")
-sub_2 = sub_2 or sub_alloc (iot_bus, s7_sub_callback, "xrt/devices/s7/reply")
+modbus_sub = modbus_sub or sub_alloc (iot_bus, modbus_sub_callback, "xrt/devices/modbus/reply")
+s7_sub = s7_sub or sub_alloc (iot_bus, s7_sub_callback, "xrt/devices/s7/reply")
 
 pub = pub or pub_alloc (iot_bus, "xrt/influxdb/message")
