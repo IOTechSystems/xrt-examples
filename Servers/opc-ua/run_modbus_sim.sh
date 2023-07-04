5f27b28930d15084611f09d302c4b6349b8f12ab
Joe Riemersma <joepaz200@gmail.com>
2023-07-04 16:19:57 +0100

    save modbus demo

--- Servers/opc-ua/run_modbus_sim.sh
+++ Servers/opc-ua/run_modbus_sim.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-docker run -it --network=host -v $(pwd)/deployment/profiles/:/profiles/ iotechsys/pymodbus-sim:1.0 --profile /profiles/CATL_BMS_profile.json --port 1502
+docker run -it --network=host -v $(pwd)/modbus-sim/:/profiles/ iotechsys/pymodbus-sim:1.0 \
+  --profile /profiles/CATL_BMS_profile.json  \
+  --port 1502  \
+  --defaults /profiles/default_values_CATL_BMS_profile.json \
+  --script /profiles/set_values_script \
+  --delay 1
