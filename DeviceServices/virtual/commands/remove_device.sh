27465921fe23ab88dbe21ffc7fc1cd7e9bb55bba
BinduRao <41687834+BinduRao2018@users.noreply.github.com>
2023-07-11 16:26:53 +0100

    XRT-2315: Updated configs to follow sparkplug hierarchy (#230)
    * XRT-2315: Update to sparkplug compliant topic configuration
    
    * XRT-2315: Bacnet_ip config update
    
    * XRT-2315: Bacnet-mstp config update for sparkplug topics
    
    * XRT-2315: Update bacnet-ip config
    
    * XRT-2315: Update bacnet-ip config
    
    * XRT-2315: update bacnet-ip configs
    
    * XRT-2315: Update bacnet-ip config
    
    * XRT-2315: Update bacnet-mstp config
    
    * XRT-2315: Update ble config
    
    * XRT-2315: Update modbus-rtu config
    
    * XRT-2315: Update modbus-tcp config
    
    * XRT-2315: ethernet-ip config update
    
    * XRT-2315:Update opc-ua config
    
    * XRT-2315: Update gps config
    
    * XRT-2315: S7 config update
    
    * XRT-2315: Virtual device service config updates
    
    * XRT-2315: Update ethercat config
    
    * XRT-2315: Remove 'AppStateWait' from sparkplug config
    
    * XRT-2315: Update opc-ua config
    
    * XRT-2315: gps config update
    
    * XRT-2315: gps config update
    
    * XRT-2315: Zigbee and multiple-services config update
    
    * XRT-2315: mqtt exporter config update
    
    * XRT-2315: update mqtt exporter config
    
    * XRT-2315: mqtt - exporter config update
    
    * XRT-2315: mqtt exporter config update
    
    * XRT-2315: mqtt - exporter config update
    
    * XRT-2315: Update bacnet-ip config
    
    * XRT-2315: virtual device service config update
    
    * XRT-2315: Config updates
    
    * XRT-2315:
    
    * XRT-2315:
    
    * XRT-2315: Update configs
    
    * XRT-2315: Config updates
    
    * XRT-2315: Config updates
    
    * XRT-2315: Update opc-ua config
    
    * XRT-2315: Multiple-service config update
    
    * XRT-2315: Fix device service commands
    
    * XRT-2315: update zigbee2mqtt config
    
    * XRT-2315: Update ethernet-ip config
    
    * XRT-2315:MQTT Bridge config update
    
    * XRT-2315: Update mqtt config
    
    * XRT-2315: Update mqtt configs
    
    * XRT-2315: Update mqtt config

--- DeviceServices/virtual/commands/remove_device.sh
+++ DeviceServices/virtual/commands/remove_device.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-mosquitto_pub -t xrt/devices/virtual/request -m \
+mosquitto_pub -t spBv1.0/${SPARKPLUG_GROUP}/REQUEST/${SPARKPLUG_NODE}/virtual -m \
 '{
   "client": "example",
   "request_id": "1011",
