7aef173eab3ee50436b5ece521f1d7c33e65281b
alasdair <aim289@outlook.com>
2023-07-19 09:25:34 +0100

    XRT-2386: Change modbus sim to pymodbus and update rtu profile

--- DeviceServices/modbus-tcp/commands/start_device_sim.sh
+++ DeviceServices/modbus-tcp/commands/start_device_sim.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-docker run --rm -d --name modbus-sim iotechsys/modbus-sim:1.0
+docker run --rm -d --name pymodbus-sim iotechsys/pymodbus-sim:1.0 --profile example_profiles/modbus-example-profile-3.0.json --port 1502
 
