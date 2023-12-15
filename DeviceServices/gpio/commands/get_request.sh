a5c9937eb65d329b2c70f2df5bfede2f40f44edf
br <bindu@iotechsys.com>
2023-12-15 10:24:20 +0000

    XRT-2544 : Update gpio example config

---
+++ DeviceServices/gpio/commands/get_request.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+mosquitto_pub -t spBv1.0/iotech/REQUEST/xrt/gpio -m \
+'{
+  "client": "example",
+  "request_id": "1020",
+  "op": "device:get",
+  "type": "xrt.request:1.0",
+  "device": "device-dio0",
+  "resource": "Channel0"
+}'
