560e518b3eb1ceab26b34d1483348bda03624744
jwcodeuk <jonathan@iotechsys.com>
2024-01-05 07:57:16 +0000

    revert

--- DeviceServices/virtual/commands/get_request.sh
+++ DeviceServices/virtual/commands/get_request.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
-mosquitto_pub -t spBv1.0/${SPARKPLUG_GROUP}/REQUEST/${SPARKPLUG_NODE}/virtual -m \
-'{"schedule": {"name": "Virtual-Schedule", "device": "Virtual-Device", "resource": ["random_sbyte3"], "interval": 1000}, "client": "XRT Python Test", "request_id": "56ec754a-75af-4aed-bd24-b5bbbac889ce", "op": "schedule:add"}
-'
+mosquitto_pub -t xrt/devices/virtual/request -m \
+'{
+  "client": "example",
+  "request_id": "1020",
+  "op": "device:get",
+  "device": "Random-Device",
+  "resource": ["RandomInt16"]
+}'
