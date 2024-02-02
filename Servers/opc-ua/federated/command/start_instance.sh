21b2d86b41391bdc972d19edbfb3fb6fe6aa52ad
jwcodeuk <jonathan@iotechsys.com>
2024-02-02 09:51:55 +0000

    XRT-2784: fixed path in start_instance.sh for copying template

--- Servers/opc-ua/federated/command/start_instance.sh
+++ Servers/opc-ua/federated/command/start_instance.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-if [ $# -ne 2 ]; then 
-    echo "Usage: $0 <xrt_executable> <instance-id>" 
-    exit 1 
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 <xrt_executable> <instance-id>"
+    exit 1
 fi
 
 xrtexe=$1
@@ -10,11 +10,17 @@ serverid="xrt-instance-$2"
 tmpdir="/tmp/$serverid"
 
 rm -rf $tmpdir
-mkdir $tmpdir 
-cp -r ./../instance_template/* $tmpdir
+mkdir $tmpdir
 
+current_dir_name=$(basename "$(pwd)")
+echo ${current_dir_name}
+
+if [ "${current_dir_name}" = "command" ]; then
+    cp -r ./../instance_template/* $tmpdir/
+else
+    cp -r ./instance_template/* $tmpdir/
+fi
 export XRT_SERVER_ID=$serverid
 cd $tmpdir
 ls
 $xrtexe ./config
-
