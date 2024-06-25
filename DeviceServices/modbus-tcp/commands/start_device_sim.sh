020a6686b1420c79936c6a8b415351d805744a10
Jonathan White <Jonathan@iotechsys.com>
2024-06-25 16:22:44 +0100

    Update start_device_sim.sh

--- DeviceServices/modbus-tcp/commands/start_device_sim.sh
+++ DeviceServices/modbus-tcp/commands/start_device_sim.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 BASEDIR=$(dirname "$0")
-docker run --rm -d -v $BASEDIR/../deployment/profiles:/profiles --name pymodbus-sim iotechsys/pymodbus-sim:1.0.5 --profile /profiles/modbus-sim-profile.json --port 1502
+docker run --rm -d -v $BASEDIR/../deployment/profiles:/profiles --name pymodbus-sim iotechsys/pymodbus-sim:1.0 --profile /profiles/modbus-sim-profile.json --port 1502
