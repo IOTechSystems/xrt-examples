c6189f7582f93d8d2ae2e558865426c989f594b7
Jonathan White <Jonathan@iotechsys.com>
2024-06-25 16:23:23 +0100

    Update start_device_sim.sh

--- DeviceServices/modbus-rtu/commands/start_device_sim.sh
+++ DeviceServices/modbus-rtu/commands/start_device_sim.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-docker run --rm -d --name pymodbus-sim iotechsys/pymodbus-sim:1.0.5 --profile example_profiles/modbus-example-profile-3.0.json --comm serial --serial_over_tcp_port 50103
+docker run --rm -d --name pymodbus-sim iotechsys/pymodbus-sim:1.0 --profile example_profiles/modbus-example-profile-3.0.json --comm serial --serial_over_tcp_port 50103
 sleep 4
 MODBUS_SIM_ADDRESS=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' pymodbus-sim)
 (socat pty,link=/tmp/virtualport,raw,echo=0 tcp:${MODBUS_SIM_ADDRESS}:50103) &
