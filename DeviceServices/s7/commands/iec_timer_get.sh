7c79e38ebfb3001c81701b6f8978131797029dbc
tylerg2471 <tylerg2471@gmail.com>
2024-09-03 13:59:08 +0100

    XRT-2976: port forward 2.2 s7 examples to 2.3

---
+++ DeviceServices/s7/commands/iec_timer_get.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+mosquitto_pub -t xrt/devices/s7/request -m \
+'{
+  "client": "example",
+  "request_id": "1020",
+  "op": "device:get",
+  "type": "xrt.request:1.0",
+  "device": "s7-sim",
+  "resource": "IEC_TIMER"
+}'
